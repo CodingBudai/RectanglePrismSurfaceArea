fb36bb0364d4fa094b2c803a962d42f463d9d7f7
Adrian Cleofe <rastaman8693@gmail.com>
2014-08-21 19:42:06 +0800

    Update RectangleController.java

--- SRC/RectangleController.java
+++ SRC/RectangleController.java
@@ -9,23 +9,32 @@ public class RectangleController {
 	public RectangleController(RectangleModel model, RectangleView view){
 		this.model = model;
 		this.view = view;
-		this.view.addComputeListener(new ComputeListener());
+		this.view.addListener(new ComputeListener());
 	}
 	
 	class ComputeListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			double height, width, depth;
+			if(e.getSource() == view.computeButton){
+				double height, width, depth;
+				
+				height = view.getRectangleHeight();
+				width = view.getRectangleWidth();
+				depth = view.getRectangleDepth();
+				
+				if(depth == 0)
+					model.computeSurfaceArea(height, width);
+				else
+					model.computeSurfaceArea(height, width, depth);
+				
+				view.setResult(model.getSurfaceArea());
+			}
 			
-			height = view.getRectangleHeight();
-			width = view.getRectangleWidth();
-			depth = view.getRectangleDepth();
-			
-			if(depth == 0)
-				model.computeSurfaceArea(height, width);
-			else
-				model.computeSurfaceArea(height, width, depth);
-			
-			view.setResult(model.getSurfaceArea());
+			if(e.getSource() == view.clearButton){
+				view.setRectangleHeight(model.clrText());
+				view.setRectangleWidth(model.clrText());
+				view.setRectangleDepth(model.clrText());
+				view.setResult(model.clrResultLabel());
+			}
 		}
 	}
 }
